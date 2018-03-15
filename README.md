ecd43d2e8b9104ea206b36846970a15fa6c309bb
elpollodiablo84 <pasqualimar@gmail.com>
2018-03-15 17:39:28 +0100

    Cleaning and corrections

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # atom.io language-matlab-plus package
-
 Atom.io language support for Matlab originally converted from TextMate Bundle: https://github.com/textmate/matlab.tmbundle.
 
 Forked from https://github.com/thedavidprice/language-matlab-octave.
 ## Planned Features
-- Code corrections and cleaning
+- Code corrections and cleaning (*Almost done*)
 - Better snippets
-- Persistent variables highlighting like in the default MATLAB editor (I'll try, at least)
+- Persistent variables highlighting like in the default MATLAB editor (*I think it can be done*)
+- Code section (%%) folding (*I don't know if it's possible*)
--- test/test1.m
+++ test/test1.m
@@ -4,8 +4,9 @@ A = {B, (C + D)'}';
 A = [B, C']';
 
 %% Strings
-fprintf(fid, "test1 + ""%s + ''test%.5f", "test2", 3e-2};
-cmd = sprintf('test1 + ""%s + ''test%.5f', 'test2', 3e-2};
+fprintf(fid, "test1 + ""%s + ''test%.5f", "test2", 3e-2};  % Escaping "" in string
+cmd = sprintf('test1 + ""%s + ''test%.5f', 'test2', 3e-2}; % Escaping '' in char array
+!shutdown
 
 %% Function
 function out = fun(in)
@@ -21,3 +22,12 @@ end
 %% 
 C = 1; % <-- This line shouldn't be a comment if you put a space after %%
 A = 3*C;
+
+%% If/elseif/else
+if (condition == 1)
+    body = 1;
+elseif (condition == 2)
+    body = 2;
+else
+    body = 3;
+end
