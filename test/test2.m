4e4a3b460edf666bdb44dce34495f01e6df96fa0
elpollodiablo84 <pasqualimar@gmail.com>
2018-04-05 14:18:05 +0200

    Grammar corrections and function names in scopes.

--- test/test2.m
+++ test/test2.m
@@ -1,14 +1,26 @@
 %% Function
-function out = fun(in, index) % Comment
+function out = test2(in, index_in) % Comment
     persistent a index c % Comment
     if isempty(a)
         a = 1;
+        c = a;
     end
 
     in = in.^a;
-    index = index + 1;
-    a = a*3;
+    index = index_in + 1;
+    a = a*3 + c;
 
-    %% Test comment in function
-    out = in(index);
+    %% Test section in function
+    out = new_fun(index, in);
+
+    %% Test function in function
+    function y = new_fun(x, z)
+        persistent y_old
+        if isempty(y_old)
+            y_old = 0;
+        end
+
+        y = x.^z + y_old + a;
+        y_old = y;
+    end
 end
